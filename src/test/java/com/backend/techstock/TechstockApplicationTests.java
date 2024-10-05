3f2b24a17bd2a32a01ea7be2a776187bfec7f9cd
devAndreLA <andre.alves@alunos.ufersa.edu.br>
2024-10-05 11:25:55 -0300

    firs commit

---
+++ src/test/java/com/backend/techstock/TechstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.techstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
