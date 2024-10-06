7d1e562713d2824021b05d76f821c324b5095d41
devAndreLA <andre.alves@alunos.ufersa.edu.br>
2024-10-06 16:25:26 -0300

    bug in post user with verification

--- src/main/java/com/backend/techstock/controller/UsersController.java
+++ src/main/java/com/backend/techstock/controller/UsersController.java
@@ -1,12 +1,16 @@
 package com.backend.techstock.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,15 +25,29 @@ public class UsersController {
     private JdbcClient jdbcClient;
 
     @GetMapping
-    public List<users> listUsers() {
+    public List<users> listAllUsers() {
         UsersModel usersModel = new UsersModel(jdbcClient);
         return usersModel.findAll();
     }
 
-    @PostMapping
-    public ResponseEntity insertUser(@RequestBody users user){
+    @GetMapping("/{nameUser}")
+    public Optional<users> listUserByName(@PathVariable String nameUser) {
         UsersModel usersModel = new UsersModel(jdbcClient);
-        usersModel.create(user);
-        return ResponseEntity.ok().build();
+    
+        return usersModel.findByName(nameUser);
+        
+    }
+
+    @PostMapping
+    public ResponseEntity insertUser2(@RequestBody users user) {
+        UsersModel usersModel = new UsersModel(jdbcClient);
+        try {
+            boolean test = usersModel.findByName(user.name()).isEmpty(); 
+            
+            return ResponseEntity.ok("O usuário já existe");
+        } catch (NullPointerException e) {
+            usersModel.create(user);
+            return ResponseEntity.ok("Usuário criado com sucesso");
+        }
     }
 }
--- src/main/java/com/backend/techstock/model/UsersModel.java
+++ src/main/java/com/backend/techstock/model/UsersModel.java
@@ -1,6 +1,9 @@
 package com.backend.techstock.model;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +17,30 @@ public class UsersModel {
         this.jdbcClient = jdbcClient;
     }
 
+    /*public Optional<users> findByName(String name){
+        return Optional.ofNullable(jdbcClient.sql("SELECT * FROM users WHERE name = :name")
+                         .param("name", name)
+                         .query(users.class)
+                         .single());
+    }*/
+
+    public Optional<users> findByName(String name) {
+        Optional<users> user;
+        
+        try {
+           user = Optional.ofNullable(jdbcClient.sql("SELECT * FROM users WHERE name = :name")
+                          .param("name", name)
+                          .query(users.class)
+                          .single());
+            
+        } catch (DataAccessException ex) {
+           user = null; // Se não encontrar no banco de dados, retorna
+        }
+
+        return user;
+    }
+
+
     public List<users> findAll(){
         return jdbcClient.sql("SELECT * FROM users").query(users.class).list();
     }
@@ -22,6 +49,20 @@ public class UsersModel {
         return jdbcClient.sql("INSERT INTO users(name, password) VALUES (:name, :password)")
                          .param("name", user.name())
                          .param("password", user.password())
+                         .update();
+    }
+
+    public Integer nameUpdate(users user){
+        return jdbcClient.sql("UPDATE users SET name = :name WHERE id = :id")
+                         .param("name", user.name())
+                         .param("id", user.id())
+                         .update();
+    }
+
+    public Integer passwordUpdate(users user){
+        return jdbcClient.sql("UPDATE users SET password = :password WHERE id = :id")
+                         .param("name", user.password())
+                         .param("id", user.id())
                          .update();
     }
 }
